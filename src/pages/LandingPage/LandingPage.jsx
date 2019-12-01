ea788ab96274de259745645b36004e192d43e866
Reshawn Ramjattan <reshawnramjattan@gmail.com>
2019-12-01 00:39:46 -0400

    added vid

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -64,7 +64,7 @@ class LandingPage extends React.Component {
                 <Button
                   color="danger"
                   size="lg"
-                  href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                  href="https://youtu.be/S7ocHSKy4x8"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
